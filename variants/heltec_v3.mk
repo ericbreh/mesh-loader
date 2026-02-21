b328cd532b33b1bbb847eda10bd73f41d7f6239b
Eric Chuang <ericchuang94@gmail.com>
2026-02-20 18:10:30 -0800

    heltec v3

---
+++ variants/heltec_v3.mk
@@ -0,0 +1,9 @@
+# Partition layout for this variant
+PARTITION_LAYOUT = 8mb_default
+
+# PlatformIO environment names for each firmware component
+MESHTASTIC_ENV = heltec-v3
+MESHCORE_ENV = Heltec_v3_companion_radio_ble
+LOADER_ENV = heltec_v3
+
+CHIP_TYPE = esp32s3
